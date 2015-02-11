ebd9acbc237ccf81203395bcd165ca5ab368eaa8
Ben Johnson <ben@bjmfactory.com>
2015-02-11 14:02:40 -0600

    map function

--- src/underbar.js
+++ src/underbar.js
@@ -114,10 +114,19 @@
 
 
   // Return the results of applying an iterator to each element.
+  // map() is a useful primitive iteration function that works a lot
+  // like each(), but in addition to running the operation on all
+  // the members, it also maintains an array of results.
   _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
+    var results = [];
+    if (Array.isArray(collection))
+      for (var i = 0; i < collection.length; i++)
+        results[i] = iterator(collection[i]);
+    else
+      for (var i in collection)
+        results[i] = iterator(collection[i]);
+
+    return results;
   };
 
   /*
